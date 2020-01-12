581a3d8d5cbf1eca36dc4c551cedd168d53cb68c
Patrik Nertigel <nertigel14475@gmail.com>
2020-01-12 14:17:25 +0200

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -100,7 +100,9 @@ Citizen.CreateThread(function()
 					displayText = _U('press_button', displayText)
 				end
 
-				ESX.Game.Utils.DrawText3D(doorID.textCoords, displayText, size)
+				--ESX.Game.Utils.DrawText3D(doorID.textCoords, displayText, size)
+
+				DrawText3D(doorID.textCoords['x'], doorID.textCoords['y'], doorID.textCoords['z'], displayText)
 
 				if IsControlJustReleased(0, 38) then
 					if isAuthorized then
@@ -117,6 +119,27 @@ Citizen.CreateThread(function()
 		end
 	end
 end)
+
+function DrawText3D(x,y,z, text)
+  local onScreen, _x, _y = World3dToScreen2d(x, y, z)
+  local p = GetGameplayCamCoords()
+  local distance = GetDistanceBetweenCoords(p.x, p.y, p.z, x, y, z, 1)
+  local scale = (1 / distance) * 2
+  local fov = (1 / GetGameplayCamFov()) * 100
+  local scale = scale * fov
+  if onScreen then
+		SetTextScale(0.35, 0.35)
+		SetTextFont(4)
+		SetTextProportional(1)
+		SetTextColour(255, 255, 255, 215)
+		SetTextEntry("STRING")
+		SetTextCentre(1)
+		AddTextComponentString(text)
+		DrawText(_x,_y)
+		local factor = (string.len(text)) / 370
+		DrawRect(_x,_y+0.0125, 0.015+ factor, 0.03, 41, 11, 41, 68)
+    end
+end
 
 function IsAuthorized(doorID)
 	if ESX.PlayerData.job == nil then
