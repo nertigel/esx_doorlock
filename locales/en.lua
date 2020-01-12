d350179f3863e0518c86e4635490f2f223c91193
Patrik Nertigel <nertigel14475@gmail.com>
2020-01-12 14:17:01 +0200

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -1,5 +1,5 @@
 Locales ['en'] = {
-  ['unlocked'] = '~g~Unlocked~s~',
-  ['locked'] = '~r~Locked~s~',
-  ['press_button'] = '[E] %s',
+  ['unlocked'] = 'Unlocked',
+  ['locked'] = 'Locked',
+  ['press_button'] = '[E] - %s',
 }
